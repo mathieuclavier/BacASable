f5ee5daf9224e1f0e7fc27e3f18f6e32e64ac507
Mathieu <matyou258@gmail.com>
2018-11-14 16:13:52 +0100

    Prmière version carre

---
+++ Numwork.py
@@ -0,0 +1,7 @@
+from kandinsky import *
+
+def carre()
+    for i in range(100)
+    set_pixel(i,i,color(0,250,0))
+    set_pixel(i+100,100, color(0,250,0))
+    set_pixel(i+100,i-100, color(0,250,0))
