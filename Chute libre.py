759bff8121ae37e17220dfc6357c3dbf624f978e
Mathieu <matyou258@gmail.com>
2018-11-30 19:24:52 +0100

    V1

---
+++ Chute libre.py
@@ -0,0 +1,12 @@
+from math import*
+
+g=float(input("Acceleration gravitationnelle: "))
+t=0
+t=float(input("Temps de chute: "))
+
+d=round((1/2*g)*t*t,1)
+print("La distance de chute parcourue par le projectile est:",d,"m")
+
+n=int(input("Arrondir la veleur à combien de chiffres ?:"))
+d=round((1/2*g)*t*t,n)
+print("La distance de chute parcourue par le projectile est:",d,"m")
