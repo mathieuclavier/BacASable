82b2f5083862052c01ef52a31bbdc39ea3865299
Mathieu <matyou258@gmail.com>
2018-11-30 20:50:24 +0100

    V1

---
+++ Portée balistique 2.py
@@ -0,0 +1,13 @@
+from math import*
+
+v=float(input("Vélocitée initiale: "))
+a=float(input("Angle de tire: "))
+g=float(input("Atraction gravitationnelle: "))
+y=float(input("Hauteur initiale/au sol: "))
+
+d=round(((v/cos(a))/g)*(v*sin(a)+sqrt((v*sin(a))*(v*sin(a))+2*g*y)),1)
+print("La distance parcourue par le projectile est:",d,"m")
+
+x=int(input("A combien de chiffre voulez-vous arrondir?: "))
+d=round(((v/cos(a))/g)*(v*sin(a)+sqrt((v*sin(a))*(v*sin(a))+2*g*y)),x)
+print("La distance parcourue par le projectile est",d,"m")
