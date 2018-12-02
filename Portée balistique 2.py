078b151271a5658b88401475251e0e4c91633e41
Mathieu <matyou258@gmail.com>
2018-12-02 10:11:56 +0100

    v2

--- Portée balistique 2.py
+++ Portée balistique 2.py
@@ -8,6 +8,12 @@ y=float(input("Hauteur initiale/au sol: "))
 d=round(((v/cos(a))/g)*(v*sin(a)+sqrt((v*sin(a))*(v*sin(a))+2*g*y)),1)
 print("La distance parcourue par le projectile est:",d,"m")
 
-x=int(input("A combien de chiffre voulez-vous arrondir?: "))
-d=round(((v/cos(a))/g)*(v*sin(a)+sqrt((v*sin(a))*(v*sin(a))+2*g*y)),x)
-print("La distance parcourue par le projectile est",d,"m")
+reponse = input("Voulez-vous arrondir plus précisément? [o/N] ")
+reponse = reponse.strip().lower()
+
+if reponse.startswith('o'):
+    x=int(input("A combien de chiffre voulez-vous arrondir?: "))
+    d=round(((v/cos(a))/g)*(v*sin(a)+sqrt((v*sin(a))*(v*sin(a))+2*g*y)),x)
+    print("La distance parcourue par le projectile est",d,"m")
+else:
+    print("La distance parcourue par le projectile est",d,"m")
