f2d259cbdcc75aadf3d8d8ac81e250fe18926841
mathieuclavier <44637776+mathieuclavier@users.noreply.github.com>
2018-12-04 09:52:08 +0100

    Update Carre 2.py

--- Carre 2.py
+++ Carre 2.py
@@ -3,9 +3,11 @@ from kandinsky import*
 def carre(x1,y1,x2,y2):
     for i in range(100): 
         set_pixel(x1, i, color(0,0,0))
+    for i in range(100):
+        set_pixel(x2, i, color(0,0,0))
+    for i in range(100):
+        set_pixel(i, y1, color(0,0,0))
+    for i in range(100):
+        set_pixel(i, y2, color(0,0,0))
         
-
-
-carre(50,10,50,50)
-
 input()
