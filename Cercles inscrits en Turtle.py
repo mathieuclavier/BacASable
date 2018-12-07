62ca2110730403266d702cb3edc833a0d309980c
mathieuclavier <44637776+mathieuclavier@users.noreply.github.com>
2018-12-07 13:26:36 +0100

    Update Cercles inscrits en Turtle.py

--- Cercles inscrits en Turtle.py
+++ Cercles inscrits en Turtle.py
@@ -1,10 +1,23 @@
 from turtle import*
+
 x=int(input("Nombre de côtés: "))
 l=int(input("Longueur de côté: "))
 n=int(input("Nombre de cercles: "))
+e=1
+
+reponse = input('Dimentions des cercles exponentielle? [o/N] ')
+reponse = reponse.strip().lower()
+    
 for i in range(n):
   for i in range(x):
     forward(l)
     left(360/x)
-  x=x-10
+  x=x-10*e
+  
+if reponse.starwith ('o'):
+  e=e+1
+else:
+  input()
+  
+
   
