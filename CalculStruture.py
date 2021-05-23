81460c1be8cc76f41411faa66b2eca042a39ac71
mathieuclavier <44637776+mathieuclavier@users.noreply.github.com>
2021-05-23 15:25:11 +0200

    Update CalculStruture.py

--- CalculStruture.py
+++ CalculStruture.py
@@ -1,1 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
+monApp=QApplication(sys.argv)
+w=QWidget()
+w.resize(500,300)
+w.move(500, 500)
+w.setWindowTitle("Titre de fenêtre")
+w.show()
+
+sys.exit(monApp.exec_())
